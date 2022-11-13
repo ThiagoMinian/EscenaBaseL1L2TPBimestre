00b7480382f48f0f09c367d9bc0523dd48fea60d
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-11-13 22:40:27 +0100

    .

---
+++ Assets/AgentScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    [SerializeField] Transform destinationTransform;
+    public float remainingDistance;
+    public bool isStopped;
+
+    [SerializeField] Transform[] targets;
+    [SerializeField] int currentTarget = 0;
+    [SerializeField] float arrivingDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+
+
+
+    }
+
+    void Update()
+    {
+        agent.destination = targets[currentTarget].position;
+        Patrullaje();
+
+    }
+
+    public void Patrullaje()
+    {
+        if (Vector3.Distance(transform.position, targets[currentTarget].position) < arrivingDistance)
+        {
+            if (currentTarget < targets.Length-1)
+            {
+                currentTarget++;
+            }
+            else
+            {
+                currentTarget = 0;
+            }
+
+        }
+    }
+}
